ad6ed367e260cedd79fa3e4d8a2f734159f0bc40
Lycheejam <lycheejamwow@gmail.com>
2019-01-27 02:55:08 +0900

    add comment to 実習9-1

--- Chapter9/NewsReader/NewsReader/ListViewController.swift
+++ Chapter9/NewsReader/NewsReader/ListViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ListViewController: UITableViewController {
+    //各セクションにセルが何個あるかを指定する。
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //セル個数の指定
         return 3
     }
     
+    //セルの内容を指定する。
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //indexPath = セクション位置, セル(Row)の位置
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        //セルの内容
         return cell
     }
 }
