909c811cfc4073a156ba934e4558e8e224f2a8b5
Lycheejam <lycheejamwow@gmail.com>
2019-01-27 02:57:36 +0900

    add Item Class to 実習9-2

---
+++ Chapter9/NewsReader/NewsReader/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by Z on 2019/01/27.
+//  Copyright © 2019 Z. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""  //記事タイトル
+    var link = ""   //記事URL
+    
+}
