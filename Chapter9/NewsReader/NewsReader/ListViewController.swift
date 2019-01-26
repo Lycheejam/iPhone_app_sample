770fd8aa4a9436b2b995b5461e326cd5d1e4202d
Lycheejam <lycheejamwow@gmail.com>
2019-01-27 03:18:23 +0900

    実習9-2

--- Chapter9/NewsReader/NewsReader/ListViewController.swift
+++ Chapter9/NewsReader/NewsReader/ListViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ListViewController: UITableViewController {
+class ListViewController: UITableViewController, XMLParserDelegate {
+    
+    var parser:XMLParser!
+    var items = [Item]()
+    var item:Item?
+    var currentString = ""
+    
     //各セクションにセルが何個あるかを指定する。
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //セル個数の指定
-        return 3
+        return items.count
     }
     
     //セルの内容を指定する。
@@ -20,7 +26,52 @@ class ListViewController: UITableViewController {
         //indexPath = セクション位置, セル(Row)の位置
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
+        cell.textLabel?.text = items[indexPath.row].title
         //セルの内容
         return cell
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        startDownload()
+    }
+    
+    func startDownload() {
+        self.items = []
+        if let url = URL(string: "https://www.apple.com/ja/newsroom/rss-feed.rss") {
+            if let parser = XMLParser(contentsOf: url) {
+                self.parser = parser
+                self.parser.delegate = self
+                self.parser.parse()
+            }
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        self.currentString = ""
+        if elementName == "item" {
+            self.item = Item()
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        self.currentString += string
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        switch elementName {
+        case "title":
+            self.item?.title = currentString
+        case "link":
+            self.item?.link = currentString
+        case "item":
+            self.items.append(self.item!)
+        default:
+            break
+        }
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        self.tableView.reloadData()
     }
 }
